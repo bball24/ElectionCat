2b8b9efcc52bed100eb75c64cd140882abaef8e5
Jack Kolb <jackhunterkolb@gmail.com>
2018-03-04 01:33:39 +0000

    seeing if admin_key works

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def route_start_voting():
 @app.route('/start_voting', methods=['POST'])
 def start_voting():
     admin_key = request.form['admin_key']
-    return "Hello"
+    return "Hello: " + str(admin_key)
     if admin_key == settings["admin_key"]:
         global voting_open
         voting_open = True
